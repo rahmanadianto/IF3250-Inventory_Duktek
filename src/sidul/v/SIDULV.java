938dd9e128c83b452a7a85100b230c64cc6f3de9
Rahman Adianto <rahman.adianto@gmail.com>
2016-01-31 01:18:10 +0700

    tester for entity class

--- src/sidul/v/SIDULV.java
+++ src/sidul/v/SIDULV.java
@@ -5,7 +5,16 @@
  */
 package sidul.v;
 
+import data.entity.Barang;
+import helper.TimeHelper;
+
 import connector.Connector;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.ResultSet;
 
 /**
  *
@@ -17,10 +26,30 @@ public class SIDULV {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
     Connector Conn = new Connector();
-    Conn.connect();
-    System.out.println("aaaaa");
+    Connection conn = Conn.connect();
+    Barang barang = null;
+   
+    try {
+        String query = "SELECT * FROM barang WHERE id=?";
+        PreparedStatement stmt = conn.prepareStatement(query);
+
+        stmt.setInt(1, 1);
+        ResultSet rs = stmt.executeQuery();
+        
+        while (rs.next()) {
+            barang = new Barang(rs.getInt("id"),
+                                rs.getString("kode"),
+                                rs.getString("nama"),
+                                rs.getString("deskripsi"));
+        }
+        
+        System.out.println(TimeHelper.TimestampToString(TimeHelper.getCurrentTimeStamp()));
+        System.out.println(barang.getNama());
+    } 
+    catch (SQLException ex) {
+        Logger.getLogger(SIDULV.class.getName()).log(Level.SEVERE, null, ex);
+    }
   }
   
 }
